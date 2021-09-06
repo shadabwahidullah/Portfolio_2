d5a9ed341de065db5acd9afa8ed8d39cc428abee
sergiogval <67393791+sergiogval@users.noreply.github.com>
2021-09-06 17:43:41 -0500

    Add a script to show menu

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-var test = "test";
+// SERGIO START
 
-function eventlt () {
-  document.alert("hey");
-}
-console.log(test);
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+// var menu = document.querySelector(".menu");
+// var menuBbtn__burger = document.querySelector(".menuBbtn__burger");
+// var menuLinks = document.querySelectorAll(".menuLink");
+
+// menuBbtn__burger.addEventListener("click", toggleMenu);
+
+// function toggleMenu() {
+//   if (menu.classList.contains('open')) {
+//     menu.classList.remove('open');
+//   } else {
+//     menu.classList.add('open');
+//   }
+// }
+
+// menuLinks.forEach( 
+//   function(menuLink) { 
+//     menuLink.addEventListener("click", toggleMenu);
+//   }
+// );
