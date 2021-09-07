5d81f250cce63dc9940ab61274595eb3efbde701
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-08 02:49:33 +0430

    Resovle linters' errors

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-console.log('started web');
-let menubtn = document.querySelector('#menu');
+const menubtn = document.querySelector('#menu');
 
-function menubtnClicked(){
+function menubtnClicked() {
   document.querySelector('.menuexpand').classList.toggle('expand');
   document.querySelector('.menu').classList.toggle('expand');
   document.querySelector('.name').classList.toggle('expand');
@@ -9,13 +8,14 @@ function menubtnClicked(){
   document.querySelector('.toolbar').classList.toggle('height100');
 }
 // console.log(typeof.temp);
-menubtn.addEventListener('click',menubtnClicked);
+menubtn.addEventListener('click', menubtnClicked);
 
-let closemenu = document.querySelector('.closebtn');
-closemenu.addEventListener('click',menubtnClicked);
+const closemenu = document.querySelector('.closebtn');
+closemenu.addEventListener('click', menubtnClicked);
 
 // let menuitems = document.querySelector('.expitem');
 // menuitems.addEventListener('click',menubtnClicked);
 
-document.querySelectorAll('.expitem').forEach(item => {
-  item.addEventListener('click', menubtnClicked)});
+document.querySelectorAll('.expitem').forEach((item) => {
+  item.addEventListener('click', menubtnClicked);
+});
