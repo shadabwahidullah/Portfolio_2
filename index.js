86fac0854bcf1227a150be7bdc831790c7246a91
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-08 02:22:56 +0430

    Add some justification to menu

--- index.js
+++ index.js
@@ -1,54 +1,15 @@
-// SERGIO START
+console.log('started web');
+let menubtn = document.querySelector('#menu');
 
-
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    document.querySelector('.toolbar').style.height = "88vh";
-    document.querySelector('.menuitem').style.display = "flex";
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    document.querySelector('.toolbar').style.height = "50px";
-    document.querySelector('.menuitem').style.display = "none";
-    menuOpen = false;
-  }
-});
-var toolbar = document.querySelector('.toolbar');
-var menuitem = document.querySelector('.menuitem');
-menuitem.addEventListener()
-function x(menuOpen,menuitem) {
-  if (!menuOpen) {
-    menuitem.style.display = 'none';
-  }else{
-    menuitem.style.display = 'flex';
+function menubtnClicked(){
+  document.querySelector('.menuexpand').classList.toggle('expand');
+  document.querySelector('.menu').classList.toggle('expand');
+  document.querySelector('.name').classList.toggle('expand');
+  document.querySelector('.closebtn').classList.toggle('expand');
+  document.querySelector('.toolbar').classList.toggle('height100');
 }
-}
-// let menu = document.querySelector('menu'); 
+// console.log(typeof.temp);
+menubtn.addEventListener('click',menubtnClicked);
 
-// function menuOpen() {
-//   menu.style.display: block;
-// }
-
-
-// var menu = document.querySelector(".menu");
-// var menuBbtn__burger = document.querySelector(".menuBbtn__burger");
-// var menuLinks = document.querySelectorAll(".menuLink");
-
-// menuBbtn__burger.addEventListener("click", toggleMenu);
-
-// function toggleMenu() {
-//   if (menu.classList.contains('open')) {
-//     menu.classList.remove('open');
-//   } else {
-//     menu.classList.add('open');
-//   }
-// }
-
-// menuLinks.forEach( 
-//   function(menuLink) { 
-//     menuLink.addEventListener("click", toggleMenu);
-//   }
-// );
+let closemenu = document.querySelector('.closebtn');
+closemenu.addEventListener('click',menubtnClicked);
