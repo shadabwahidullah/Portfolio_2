bca73b11b6b7b9b9cb1598f6b5d0a9d57dd63b2e
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-09 19:44:41 +0430

    Change title for modal through JS

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ closemenu.addEventListener('click', menubtnClicked);
 document.querySelectorAll('.expitem').forEach((item) => {
   item.addEventListener('click', menubtnClicked);
 });
+
+let projects = {
+  first:{
+    name: 'first',
+    desc: 'first project description',
+    image: '#',
+    techs: ['html','css','ruby'],
+    live: '',
+    source: '',
+  }
+}
+console.log(projects.first.name);
+console.log(projects.first.name);
+console.log(projects.first.techs);
+document.getElementById('popupTitle').innerHTML = "nothing";
+
+// function generatePopup(){
+//   document.getElementById('popupTitle') = projects.first.name;
+// }
+
+// document.querySelector('.popup').addEventListener('click',generatePopup);
